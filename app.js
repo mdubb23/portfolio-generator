70553cdf652b09d9cc1e7d28917ee6bce3f68d28
Matthew <willivms02@hotmail.com>
2022-04-13 18:20:54 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const fs = require('fs')
+
+const generatePage = require('./src/page-templat');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
+
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
+
+
+/*
 const printProfileData = profileDataArr => {
     for (let i = 0; i< profileDataArr.length; i++) {
 
@@ -15,3 +31,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs)
+*/
